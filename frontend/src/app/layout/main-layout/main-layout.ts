76dce40b4129740889ef3ff6f84b79abb42e4d55
greadevelopment <greadevelopment@gmail.com>
2025-07-23 13:11:36 +0200

    Reconfiguración nginx

--- frontend/src/app/layout/main-layout/main-layout.ts
+++ frontend/src/app/layout/main-layout/main-layout.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,6 +14,8 @@ import { NgIf } from '@angular/common';
   styleUrls: ['./main-layout.scss'],
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
